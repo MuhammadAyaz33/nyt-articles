c6d5c0ca61aad2290b34cec7fe40ce091e634db0
user <ayazmuhammad33@gmail.com>
2024-04-16 12:30:20 +0100

    minor fixes 1

--- src/pages/articleDetailsArticleDetails.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ArticleDetails from './articleDetails';
-
-describe('<ArticleDetails />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ArticleDetails />);
-  });
-});
--- src/pages/articlesArticles.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Articles from './articles';
-
-describe('<Articles />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Articles />);
-  });
-});
